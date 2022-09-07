db46290060885bbfef06096101a990abdc81020f
arturtarasov <artur-tarasov@mail.ru>
2022-09-07 19:33:02 +0300

    added constants

--- test/voting.js
+++ test/voting.js
@@ -2,12 +2,15 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("VotingContract", function () {
+    const MAX_CANDIDATE_NUM = 10;
+    const COMISSION = 5;
+    const VOTING_PERIOD = 180;
     let owner, buyer, votingContract, accounts;
     beforeEach(async function () {
         accounts = await ethers.getSigners()
         owner = accounts[0]
         const VotingContract = await ethers.getContractFactory("VotingContract", owner)
-        votingContract = await VotingContract.deploy(10, 5)
+        votingContract = await VotingContract.deploy(MAX_CANDIDATE_NUM, COMISSION)
         await votingContract.deployed()
     })
 
@@ -16,12 +19,14 @@ describe("VotingContract", function () {
     });
 
     it("owner try to create voting with too many candidates", async function () {
-        let candidates = new Array();
+        const candidates = new Array()
+        const maxCandidatesNumError = MAX_CANDIDATE_NUM * 2
         
-        for (i = 1; i < 20; i++) candidates.push(accounts[i].address);
+        for (i = 1; i < maxCandidatesNumError; i++) candidates.push(accounts[i].address)
         await expect(
-            votingContract.connect(accounts[0]).addVoting(180, candidates)
+            votingContract.connect(accounts[0]).addVoting(VOTING_PERIOD, candidates)
         ).to.be.revertedWith("Too many candidates!");    
     });
+
 
 });
