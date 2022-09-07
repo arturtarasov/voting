4d323fbabeb476dd4bc0d39374df4da672619dc0
arturtarasov <artur-tarasov@mail.ru>
2022-09-07 19:37:53 +0300

    test: check voting counter

--- test/voting.js
+++ test/voting.js
@@ -28,5 +28,16 @@ describe("VotingContract", function () {
         ).to.be.revertedWith("Too many candidates!");    
     });
 
+    it("owner created a vote, the counter is increased", async function(){
+        const counterBefore = await votingContract.counter()
+        const candidates = new Array()
+        for (i = 1; i < MAX_CANDIDATE_NUM; i++) candidates.push(accounts[i].address)
+        await votingContract.connect(owner).addVoting(VOTING_PERIOD, candidates)
+        const counterAfter = await votingContract.counter()
+        expect(counterAfter - counterBefore).to.equal(1)
+        const isSecondCandidate = await votingContract.checkCandidate(counterBefore, accounts[1].address)
+        expect(isSecondCandidate).to.equal(true)
+    });
+
 
 });
