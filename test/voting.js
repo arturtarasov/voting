e3a0f3994f4823865261f2cd436977c6336fd86c
arturtarasov <artur-tarasov@mail.ru>
2022-09-07 19:27:50 +0300

    test: Too many candidates! error

--- test/voting.js
+++ test/voting.js
@@ -2,17 +2,26 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("VotingContract", function () {
-    let owner, buyer, voting;
+    let owner, buyer, votingContract, accounts;
     beforeEach(async function () {
-        [owner, buyer] = await ethers.getSigners()
-
+        accounts = await ethers.getSigners()
+        owner = accounts[0]
         const VotingContract = await ethers.getContractFactory("VotingContract", owner)
-        voting = await VotingContract.deploy(10, 5)
-        await voting.deployed()
+        votingContract = await VotingContract.deploy(10, 5)
+        await votingContract.deployed()
     })
 
     it("sets owner", async function () {
-        expect(await voting.owner()).to.equal(owner.address);
+        expect(await votingContract.owner()).to.equal(owner.address);
+    });
+
+    it("owner try to create voting with too many candidates", async function () {
+        let candidates = new Array();
+        
+        for (i = 1; i < 20; i++) candidates.push(accounts[i].address);
+        await expect(
+            votingContract.connect(accounts[0]).addVoting(180, candidates)
+        ).to.be.revertedWith("Too many candidates!");    
     });
 
 });
